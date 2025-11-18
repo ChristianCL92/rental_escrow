4f7a7c61e2c6d7afd666d67a5109ae4ed1fa15a4
Christian <chrisdev92@outlook.com>
2025-11-18 14:40:27 +0100

    Add token transfer functionality in escrow initialization

--- programs/rental_escrow/src/lib.rs
+++ programs/rental_escrow/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Token, TokenAccount, Transfer, Mint };
 
 declare_id!("2mGptfx2M9rTGsGExE9T3yLZ6MHSXLcgiQjD1NoVsfVa");
 
@@ -20,8 +21,19 @@ pub mod rental_escrow {
         escrow_account.rent_started =false;
         escrow_account.rent_ended = false;
 
+        let transfer_ctx = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            Transfer{
+                    from: ctx.accounts.guest_token_account.to_account_info(),
+                    to: ctx.accounts.escrow_token_account.to_account_info(),
+                    authority: ctx.accounts.guest.to_account_info(),
+            },
+          );
 
-        msg!("Escrow account for apartment {}", apartment_id);
+         token::transfer(transfer_ctx, amount)?;
+
+
+        msg!("Escrow created! {} USDC transferred to escrow", amount);
         Ok(())
     }
 }
@@ -36,17 +48,36 @@ pub struct InitializeEscrow <'info> {
     seeds = [b"escrow", guest.key().as_ref(), apartment_id.to_le_bytes().as_ref()],
     bump
 )]
-    
 pub escrow_account: Account < 'info, EscrowAccount>,
+
+#[account(
+    init,
+    payer = guest,
+    token::mint = usdc_mint,
+    token:: authority = escrow_account, 
+)]
+pub escrow_token_account: Account < 'info, TokenAccount>,
+
+#[account(
+    mut,
+    constraint = guest_token_account.owner == guest.key(),
+    constraint= guest_token_account.mint == usdc_mint.key(),
+
+)]
+pub guest_token_account: Account<'info, TokenAccount>,
+
     
 #[account(mut)]
 pub guest: Signer<'info>,
+
+pub usdc_mint: Account <'info, Mint>,
 
 ///CHECK: The property owner's wallet address, is only stored for reference.
 /// No need to validate because I am just recording when owner wallet receives payment later
 pub owner: UncheckedAccount<'info>,
 
-pub system_program: Program <'info, System>
+pub token_program: Program<'info, Token>,
+pub system_program: Program <'info, System>,
 
 }
 
