b55d3145e077216eec31cb8d726758d0911eed7e
Christian <chrisdev92@outlook.com>
2025-11-25 16:20:20 +0100

    wrap application in SolanaWalletProvider and add Navbar component

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import SolanaWalletProvider from "../provider/SolanaWalletProvider";
+import NavBar from "@/components/NavBar";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SolanaWalletProvider>
+        <NavBar/>
         {children}
+        </SolanaWalletProvider>
       </body>
     </html>
   );
