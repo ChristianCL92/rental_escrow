4fb31ebb5e8bd69a55eb77a0bc7af21238399492
Christian <chrisdev92@outlook.com>
2025-12-17 09:22:48 +0100

    refactor NavBar component to include booking link in the navigation

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,13 +1,17 @@
 "use client"
 
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui"
+import Link from "next/link"
 
 const NavBar = () => {
   
     return (
     <nav className="flex justify-between items-center p-6 bg-gray-900">
         <h1 className="text-2xl font-bold text-white">El Solar</h1>
+        <div className="flex items-center gap-10">
+        <Link href={"/bookings"} className="font-bold text-white text-md">My Bookings</Link>
         <WalletMultiButton />
+        </div>
     </nav>
   )
 }
