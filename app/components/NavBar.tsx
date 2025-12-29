f994a1a2e2b7193e657b6686a58b5e7d028bb186
Christian <chrisdev92@outlook.com>
2025-12-29 15:52:27 +0100

    include the logo to navigation bar component

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,12 +2,23 @@
 
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui"
 import Link from "next/link"
+import Image from "next/image"
+import LogoElSolar from "@/public/elsolar-logo.jpeg"
 
 const NavBar = () => {
   
     return (
     <nav className="flex justify-between items-center p-6 bg-gray-900">
-        <h1 className="text-2xl font-bold text-white">El Solar</h1>
+      <div className="flex items-center gap-2 sm:gap-5">
+        <Image 
+       src={LogoElSolar}
+       alt="El Solar Logo"
+       width={45}
+       height={45}
+       className="max-[395px]:hidden"
+       />
+        <h1 className="text-lg font-bold text-white sm:text-2xl">El Solar</h1>
+      </div>
         <div className="flex items-center gap-3 sm:gap-10">
         <Link href={"/bookings"} className="font-bold text-white text-md">My Bookings</Link>
         <WalletMultiButton />
